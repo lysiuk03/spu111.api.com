bb36485e282e18f095c456a6036f4fb3f8e2b1f4
lysiuk03 <lysiuk03@gmail.com>
2024-01-08 14:28:04 +0200

    AddComment

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *  Основний контролер додатка.
+ *  Клас, який надає базовий функціонал для інших контролерів.
+ *  Включає в себе авторизацію, валідацію та базові методи.
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Усіх із Різдвом та Новим роком!",
+ *      description="Demo my Project ",
+ *      @OA\Contact(
+ *          email="admin@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
